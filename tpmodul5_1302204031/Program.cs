dbfd7760ecb320f682b0ad82f6351fdd09c68196
Dewarjuna <100781488+Dewarjuna@users.noreply.github.com>
2022-03-25 10:41:05 +0700

    menambahkan class SayaTubeVideo

--- tpmodul5_1302204031/Program.cs
+++ tpmodul5_1302204031/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul5_1302204031
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            try
+            {
+                SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract – Dewa");
+                video.IncreasePlayCount(20);
+                video.PrintVideoDetails();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+        }
+    }
+}
---
+++ tpmodul5_1302204031/SayaTubeVideo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace tpmodul5_1302204031
+{
+    internal class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo() { }
+        public SayaTubeVideo(string judul)
+        {
+            if (judul.Length > 100 && judul == null)
+                throw new Exception("Judul kelebihan");
+            title = judul;
+            Random rnd = new Random();
+            id = rnd.Next(10000, 100000);
+            playCount = 0;
+        }
+        public void IncreasePlayCount(int jumlah)
+        {
+            
+            {
+                playCount = checked(playCount + jumlah);
+            }
+            
+            {
+                Console.WriteLine("Jumlah play count akan melampaui batas");
+            }
+        }
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("ID       :" + id);
+            Console.WriteLine("Judul    :" + title);
+            Console.WriteLine("Views    :" + playCount);
+        }
+    }
+}
